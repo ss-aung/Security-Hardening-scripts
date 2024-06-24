16daf942fc7211298cb4a09ece1da12a019fe85b
Soe San Aung  (NCS) <aung.soe.san@ncs.co>
2024-06-24 18:16:48 +0800

    0224062024

---
+++ sudogroupcheck.sh
@@ -0,0 +1,1 @@
+grep -Pi '^\h*auth\h+(?:required|requisite)\h+pam_wheel\.so\h+(?:[^#\n\r]+\h+)?((?!\2)(use_uid\b|group=\H+\b))\h+(?:[^#\n\r]+\h+)?((?!\1)(use_uid\b|group=\H+\b))(\h+.*)?$' /etc/pam.d/su
